398a91cc4fd14c2b9d43a707d3790afe9f688ced
gilberas <gillyimo2009@gmail.com>
2026-09-04 13:58:04 +0300

    feat: add table prefix to avoid collisions on shared db

--- config/database.php
+++ config/database.php
@@ -93,7 +93,7 @@ return [
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
+            'prefix' => 'shule_',
             'prefix_indexes' => true,
             'search_path' => 'public',
         ],
